869206adb2aa63be453d8a2164011ece42f8dfa9
peter <uktailwind@gmail.com>
2026-08-29 14:32:25 +0100

    chore: bump version to 6.03.02b1

--- buildpicomite.bat
+++ buildpicomite.bat
@@ -6,7 +6,7 @@ set "start_time=%time%"
 echo Build started at: %start_time%
 echo.
 
-set "fixed_string=V6.03.02b0"
+set "fixed_string=V6.03.02b1"
 set "extension=.uf2"
 set "directory=../uf2/"
 set "generator=NMake Makefiles"
